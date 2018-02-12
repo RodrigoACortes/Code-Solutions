134fe44d3ab6cb215f55fd5893dc4b27b2a807ee
Charles Velazquez <CharlesVelazquez15@gmail.com>
2018-02-12 12:45:53 -0500

    pushing up some style changes I forgot to add

--- public/javascript/shop.js
+++ public/javascript/shop.js
@@ -1,13 +1,7 @@
-var model = document.getElementsByClassName('model')[0]
-var x = document.getElementById('x')
+var buy_now = document.getElementsByClassName('buy_now')
 
-function unhide()
-{
-	model.style.display = 'block'
+for (var i = buy_now.length - 1; i >= 0; i--) {
+	buy_now[i].addEventListener('click', function () {
+		alert('Purchased')
+	})
 }
-
-setTimeout (unhide, 4000)
-x.addEventListener('click', function()
-{
-	model.style.display = 'none'
-})
